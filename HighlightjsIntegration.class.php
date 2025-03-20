40d3ebf16dc8f2aa2f315da060d511b4ebad8628
D-Groenewegen <dennisgroenewegen@gmail.com>
2025-03-20 10:56:34 +0100

    Update HighlightjsIntegration.class.php
    Guard against lang not being set

--- HighlightjsIntegration.class.php
+++ HighlightjsIntegration.class.php
@@ -53,7 +53,7 @@ class HighlightjsIntegration
             $htmlAttributes['class'] = 'code2highlight';
         }
 
-        if ($lang)
+        if ( isset( $lang ) && $lang !== '' )
         {
             $htmlAttributes['class'] .= " lang-$lang";
         }
